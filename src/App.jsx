2a84e95b43b933d28bd26090c611f5c4265fa260
gottanned <gottanned@gmail.com>
2023-09-25 09:54:08 -0500

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ export class NavBar extends Component {
     return (
       <React.Fragment>
         <nav
-          class="navbar navbar-expand-lg bg-primary navbar-style"
+          className="navbar navbar-expand-lg bg-primary navbar-style"
           data-bs-theme="dark"
         >
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/#">
               Galeria
             </a>
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -20,24 +20,27 @@ export class NavBar extends Component {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link" href="/#">
                     Home
                   </a>
                 </li>
               </ul>
-              <form class="d-flex" role="search">
+              <form className="d-flex" role="search">
                 <input
-                  class="form-control me-2"
+                  className="form-control me-2"
                   type="search"
                   placeholder="Search"
                   aria-label="Search"
                 />
-                <button class="btn btn-outline-light" type="submit">
+                <button className="btn btn-outline-light" type="submit">
                   Search
                 </button>
               </form>
